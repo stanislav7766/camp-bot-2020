7d05326f18e1f3b338827879c93ca42e65a74645
stanislav7766 <stanislavtihonov5@gmail.com>
2020-07-14 03:03:47 +0300

    added temp msgs

--- src/tools/papyrus/papyrus.js
+++ src/tools/papyrus/papyrus.js
@@ -1,5 +1,6 @@
 export const papyrus = {
   getInitialGreeting: `Приветствую! Чтобы начать работу с Bot Camp 2020, нажмите /authorize для авторизации либо /help для ознакомления возможностей бота`,
   getHelpInfo: 'тут будет помощь когда-то )0',
-  afterAuthorizeMsg: 'конгратс, ты юзер',
+  afterAuthorizeMsgUser: 'конгратс, ты юзер',
+  afterAuthorizeMsgAdmin: 'конгратс, ты админ',
 }
